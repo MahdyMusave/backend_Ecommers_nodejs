9d6f766fbf567b7f5851bb1dc12ef6b09be2a3dc
mahde13 <musavemahdy>
2023-12-25 00:55:31 -0900

    create login and register with jsonwebtoken

--- router/authRoute.js
+++ router/authRoute.js
@@ -1,7 +1,9 @@
 const express = require("express");
-const { createUser } = require("../controller/userCtrl");
 const router = express.Router();
+const { createUser } = require("../controller/userCtrl");
+const { createlogin } = require("../controller/userCtrl");
 const { findUser } = require("../controller/userCtrl");
 router.get("/", findUser);
 router.post("/register", createUser);
+router.post("/login", createlogin);
 module.exports = router;
