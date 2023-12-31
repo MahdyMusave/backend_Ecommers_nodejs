ce70fce95b02f7480f15246a233d593dd01fc48d
mahde13 <musavemahdy>
2023-12-31 06:01:05 -0900

    sort product by query

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -28,48 +28,30 @@ const createProduct = asyncHandler(async (req, res) => {
 const getAllProduct = asyncHandler(async (req, res) => {
   // console.log(req.query);
   try {
-    // const products = await Product.find({});
-    // you can search in api postman som this exmple =>urls?brand=
-    //localhost:3001/api/product?brand=Dell
-    //localhost:3001/api/product?brand=Dell&color=pink
-    /************************************ 
-    //   const products = await Product.find(req.query);
-    */
-    //and other way
-    /************************************ 
-      const products = await Product.find({
-        brand: req.query.brand,
-        category: req.query.category,
-      });
-     */
-    // and other way
+    //filtering
     const queryObj = { ...req.query };
     const excludeFields = ["page", "sort", "limit", "fields"];
-    // console.log(excludeFields);
-    // console.log(queryObj);
 
     excludeFields.forEach((el) => {
       delete queryObj[el];
     });
 
-    //console.log(queryObj, req.query);
-    // your result is this====>
-    // { brand: 'Dell', color: 'pink' } { brand: 'Dell', color: 'pink', sort: 'price' }
-    // const getallProduct = await Product.where("category").equals(
-    //   req.query.category
-    // );
-    // one other way for fix error
-
     let queryStr = JSON.stringify(queryObj);
     // console.log(queryStr);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    console.log(JSON.parse(queryStr));
-    //send http requist
-    //http://localhost:3001/api/product?price[gte]=100
-    // get result
-    // { price: { '$gte': '100' } }
-    const getallProduct = await Product.find(queryObj);
-    res.json(getallProduct);
+    // console.log(JSON.parse(queryStr));
+
+    let query = Product.find(JSON.parse(queryStr));
+
+    //sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(",").join(" ");
+    } else {
+      query = query.sort(".createdAt");
+    }
+
+    const product = await query;
+    res.json(product);
   } catch (error) {
     throw new Error(error);
   }
