9ca80a7e4b10c491b215285cefb519c9fe29317c
mahde13 <musavemahdy>
2023-12-31 06:54:26 -0900

    create page ,limit nad pagenation

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -50,6 +50,28 @@ const getAllProduct = asyncHandler(async (req, res) => {
       query = query.sort(".createdAt");
     }
 
+    //limiting the fields
+
+    if (req.query.fields) {
+      const fields = req.query.fields.split(",").join(" ");
+      query = query.select(fields);
+    } else {
+      query = query.select("-__v");
+    }
+    //for get fields by http requist
+    // http://localhost:3001/api/product?fields=title,price,category
+
+    // pagination
+    const page = req.query.page;
+    const limit = req.query.limit;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+    if (req.query.page) {
+      const productCount = await Product.countDocuments();
+      if (skip >= productCount) throw new Error("this page does not exists");
+    }
+    console.log(page, limit, skip);
     const product = await query;
     res.json(product);
   } catch (error) {
